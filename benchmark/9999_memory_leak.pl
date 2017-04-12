d62700c03dbe67f5fc556e0d1a3846b1a9dc643a
Sergey Yurzin <jurzin.s@gmail.com>
2017-04-12 14:55:59 +0300

    add memory leak test script

--- benchmark/9999_memory_leak.pl
+++ benchmark/9999_memory_leak.pl
@@ -1,54 +1,43 @@
+#!/usr/bin/perl
+
 use strict;
-use warnings;
-use Test::More;
+use warnings FATAL => 'all';
+use Test::LeakTrace;
+use Redis::Cluster::Universal;
 
 
-BEGIN {
-    my $module = 'Redis::Cluster::Universal';
-    use_ok($module);
-};
+my $cnt;
+my $module_list = [qw/Redis Redis::Fast/];
 
-SKIP: {
-    skip("Environment variable REDIS_CLUSTER isn't defined") unless ($ENV{REDIS_CLUSTER});
-
-    my $cnt;
-    my $module_list = [qw/Redis Redis::Fast/];
-
-    foreach my $module (@{$module_list})
-    {
-        eval {
-            Module::Load::load($module);
-        };
-
-        next if ($@);
-
-        $cnt++;
-
-        my $cluster_nodes = [split(/[\s,;]+/, $ENV{REDIS_CLUSTER})];
-
-        my $key = 'foo{spam}key';
-        my $value = 'Some important information';
-
-        eval {
-            require Test::LeakTrace;
-        };
-
-        skip("Can't load Test::LeakTrace module") if ($@);
-
-            Test::LeakTrace::no_leaks_ok( {
-                for (0 .. 999) {
-                    my $rcu = Redis::Cluster::Universal->new(nodes => $cluster_nodes, transport => $module);
-
-                    $rcu->set($key, $value);
-                    $rcu->get($key);
-                    $rcu->del($key);
-                }
-            } 'No memory leaks');
-
-
-    }
-
-    skip(sprintf("Can't load any of the modules: %s", join(', ', @{$module_list}))) unless ($cnt);
+unless ($ENV{REDIS_CLUSTER}) {
+    print "Environment variable REDIS_CLUSTER isn't defined\n";
+    exit(0);
 }
 
-done_testing();
+foreach my $module (@{$module_list}) {
+    eval {
+        Module::Load::load($module);
+    };
+
+    next if ($@);
+    $cnt++;
+
+    my $cluster_nodes = [split(/[\s,;]+/, $ENV{REDIS_CLUSTER})];
+
+    my $key = 'foo{spam}key';
+    my $value = 'Some important information';
+
+    my @info = leaked_info {
+        for (0 .. 9) {
+            my $rcu = Redis::Cluster::Universal->new(nodes => $cluster_nodes, transport => $module);
+
+            $rcu->set($key, $value);
+            $rcu->get($key);
+            $rcu->del($key);
+        }
+    };
+
+    print join("\n", map {index($_->[1], 'Cluster/Universal') != -1 ? join(' ', @{$_}) : ()} @info);
+}
+
+printf("Can't load any of the modules: %s", join(', ', @{$module_list})) unless ($cnt);
